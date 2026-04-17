4b5fa19237dd023efc995cbf9c946bb8b7381c6e
Gizmo <nationglory00@gmail.com>
2026-04-17 10:29:27 -0400

    refactor: delete an unnecessary  file ps:sorry

--- .py
+++
@@ -1,9 +0,0 @@
-
-
-def solve(term, first,two: list):
-    multiplier = two[1]-two[0]
-    print(multiplier)
-    return round(first+(term-1)*multiplier,4)
-
-
-print(solve(term=36, first=10, two=[17,24]))
